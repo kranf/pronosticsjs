8abe6face1a1909d80b1c890e9d47e6f0c07dd15
kranf <kranf@users.noreply.github.com>
2024-02-06 21:31:58 +0100

    Ignite mongo db

--- scripts/scrap-data.ts
+++ scripts/scrap-data.ts
@@ -1,7 +1,13 @@
+import { MongoClient } from "mongodb";
 import { PmuApiClient } from "../src/pmu-api.client";
 
 const startDate = process.argv[3]
 const pmuClient = new PmuApiClient();
+
+const dbUri = process.env['MONGODB_URI'] || ''
+const dbName = process.env['DB_NAME'] || ''
+const dbClient = new MongoClient(dbUri);
+const db = dbClient.db(dbName);
 
 pmuClient.getProgramOfTheDay(startDate).then((program) => {
     console.log(program);
