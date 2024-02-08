b92785448f3b42ee760b4fe268a0767e5e141ed6
kranf <kranf@users.noreply.github.com>
2024-02-08 21:00:28 +0100

    Let's call ddMMyyyy date as normalDate.

--- src/pmu-api.client.ts
+++ src/pmu-api.client.ts
@@ -6,29 +6,29 @@ const DETAILED_PERF_URL = 'https://online.turfinfo.api.pmu.fr/rest/client/2/prog
 export class PmuApiClient {
     /**
      * 
-     * @param date format expected to be ddMMyyyy
+     * @param normalDate format expected to be ddMMyyyy
      * e.g. 3112020 => 31st december 2020
     */
-    public async getProgramOfTheDay(date: string) {
-        const url = `${BASE_URL}/1/programme/${date}?meteo=true&specialisation=INTERNET`
-        return (await this.fetchBodyAsJson<{programme: unknown}>(url)).programme; 
+    public async getProgramOfTheDay(normalDate: string): Promise<Record<string, any>> {
+        const url = `${BASE_URL}/1/programme/${normalDate}?meteo=true&specialisation=INTERNET`
+        return (await this.fetchBodyAsJson<{programme: Record<string, any>}>(url)).programme; 
     }
 
     /**
      * Retreives participants of a race identified by 
-     * @param date as ddMMyyyy, a @param meetingId and a @param raceId 
+     * @param normalDate as ddMMyyyy, a @param meetingId and a @param raceId 
      */
-    public async getParticipants(date: string,  meetingId: string, raceId: string) {
-        const url = `${BASE_URL}/1/programme/${date}/R${meetingId}/C${raceId}/participants?specialisation=INTERNET`
+    public async getParticipants(normalDate: string,  meetingId: string, raceId: string) {
+        const url = `${BASE_URL}/1/programme/${normalDate}/R${meetingId}/C${raceId}/participants?specialisation=INTERNET`
         return this.fetchBodyAsJson<{programme: unknown}>(url); 
     }
 
     /**
      * Retreives participant last performance including the driver details of a race identified by 
-     * @param date as ddMMyyyy, a @param meetingId and a @param raceId 
+     * @param normalDate as ddMMyyyy, a @param meetingId and a @param raceId 
      */
-    public async getDetailedPerf(date: string,  meetingId: string, raceId: string) {
-        const url = `${BASE_URL}/2/programme/${date}/R${meetingId}/C${raceId}/performances-detaillees/pretty`
+    public async getDetailedPerf(normalDate: string,  meetingId: string, raceId: string) {
+        const url = `${BASE_URL}/2/programme/${normalDate}/R${meetingId}/C${raceId}/performances-detaillees/pretty`
         return this.fetchBodyAsJson<{programme: unknown}>(url); 
     }
 
