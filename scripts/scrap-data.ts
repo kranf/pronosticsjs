605e9f0ef2afc73cffca329da0f7262c7a2f0d8c
kranf <kranf@users.noreply.github.com>
2024-06-05 22:07:39 +0200

    scrap script ready

--- scripts/scrap-data.ts
+++ scripts/scrap-data.ts
@@ -4,10 +4,12 @@ import { ScrappedDataService } from "../src/scrapped-data.service.js";
 import { setLogger } from "../src/set-logger.js";
 import winston from "winston";
 import pMap from "p-map";
+import { DateIterable, NormalDate, toNormalDate } from '../src/date.utils.js';
 
 setLogger();
 
 const startDate = process.argv[2]
+const endDate = process.argv[3] || toNormalDate(new Date());
 const pmuClient = new PmuApiClient();
 
 const dbUri = process.env['MONGODB_URI'] || ''
@@ -18,25 +20,39 @@ const db = dbClient.db(dbName);
 const scrappedDataService = new ScrappedDataService(db)
 
 async function scrapData() {
-    winston.info(`Scrapping program ${startDate}`)
-    const program = await pmuClient.getProgramOfTheDay(startDate)
-    await scrappedDataService.saveProgram(program, startDate);
+    let _startDate = startDate
+    if(!startDate) {
+        const latestScrapping = await scrappedDataService.getLatestScrappingDate()
+        if (!latestScrapping) {
+            winston.error('Latest scrapping date not found')
+        }
+        winston.debug(`Latest date ${latestScrapping}`)
+        _startDate = latestScrapping!
+    }
+    const dateIterable = new DateIterable(_startDate, endDate)
+
+    return pMap(dateIterable, (date) => scrapProgramOfDay(date), {concurrency: 4})
+}
+
+async function scrapProgramOfDay(normalDate: NormalDate) {
+    winston.info(`Scrapping program ${normalDate}`)
+    const program = await pmuClient.getProgramOfTheDay(normalDate)
+    await scrappedDataService.saveProgram(program, normalDate);
 
     const everyRaces = program.reunions.flatMap((meeting) => meeting.courses)
-    pMap(everyRaces, async (race) => {
+    await pMap(everyRaces, async (race) => {
         const meetingId = race.numReunion;
         const raceId = race.numOrdre;
-        winston.info(`${startDate} - Meeting ${meetingId} - Race ${raceId} - ${race.libelle}`)
-        const participants = pmuClient.getParticipants(startDate, meetingId, raceId)
-        await scrappedDataService.saveParticipants(participants, startDate, meetingId, raceId)
+        winston.info(`${normalDate} - Meeting ${meetingId} - Race ${raceId} - ${race.libelle}`)
+        const participants = pmuClient.getParticipants(normalDate, meetingId, raceId)
+        await scrappedDataService.saveParticipants(participants, normalDate, meetingId, raceId)
 
-        const participantsDetails = pmuClient.getDetailedPerf(startDate, meetingId, raceId)
-        await scrappedDataService.saveParticipantsDetailedPerf(participantsDetails, startDate, meetingId, raceId)
+        const participantsDetails = pmuClient.getDetailedPerf(normalDate, meetingId, raceId)
+        await scrappedDataService.saveParticipantsDetailedPerf(participantsDetails, normalDate, meetingId, raceId)
     })
-    // await pmuClient.getParticipants(startDate)
-    winston.info(await scrappedDataService.getProgram(startDate));
 }
 
 scrapData().then(() => process.exit()).catch((err) => {
+    dbClient.close()
     winston.error(err);
 })
